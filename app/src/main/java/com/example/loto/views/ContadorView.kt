b6032c373dcc9496a700323c5e9ceb2a6a797bfc
Alejandro Martínez <alejandromartinezescobedo@gmail.com>
2024-12-18 18:54:53 -0600

    Actualización con cirularprogressindicator

--- app/src/main/java/com/example/loto/views/ContadorView.kt
+++ app/src/main/java/com/example/loto/views/ContadorView.kt
@@ -11,31 +11,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.loto.viewModels.ContadorViewModel
 
 @Composable
-fun Contador(viewModel: ContadorViewModel){
-    Box(contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize())
-    {
-        Text(text = viewModel.contador.value.toString(),
+fun Contador( viewModel: ContadorViewModel)
+{
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(
+            text = viewModel.contador.value.toString(),
             fontWeight = FontWeight.Bold,
             fontSize = 40.sp
+
         )
         FloatingActionButton(
-            onClick = {viewModel.add()},
+            onClick = { viewModel.add() },
             modifier = Modifier
                 .align(Alignment.BottomEnd)
                 .padding(15.dp)
-        )
-        {
+        ) {
             Icon(
                 imageVector = Icons.Default.Add,
                 contentDescription = "",
-                tint = androidx.compose.ui.graphics.Color.Blue
+                tint = Color.Blue
+
             )
         }
     }
